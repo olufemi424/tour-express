e2a2aede6058a472b733b6edb159a7b973fa5dfd
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-09-17 22:39:01 -0500

    hotel express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server !', app: 'Hotel Express' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this end point');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
