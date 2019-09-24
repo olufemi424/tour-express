f9fd4ec76a56773ca7747973d3d64ea8a3028ce9
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-09-23 22:09:32 -0500

    post route

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server !', app: 'Hotel Express' });
-// });
-
-// //Set up route to handle request and responses
-// app.post('/', (req, res) => {
-//   res.send('You can post to this end point');
-// });
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -26,6 +18,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const PORT = 3000;
