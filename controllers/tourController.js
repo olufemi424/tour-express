2a60f1ec82adee18f3814bc48f436c4ea5c4c4fa
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-02 21:03:52 -0500

    chaining middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,6 +11,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkTourBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing Tour name or Price'
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'succcess',
