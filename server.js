bb916702effdc76a3db1c1e9aa44632dfcf8ce05
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-08 20:43:48 -0500

    connect to db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
@@ -6,6 +7,15 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-yzj5q.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App running on http://localhost:${PORT}`);
+      console.log(`db connect successful`);
+    });
+  })
+  .catch(err => console.log(err));
