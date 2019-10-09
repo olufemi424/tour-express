3aeb6ecd324060617abd0e1096fa4d9354af05f5
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-08 21:40:09 -0500

    mvc architecture

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.checkTourBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -24,48 +13,28 @@ exports.checkTourBody = (req, res, next) => {
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'succcess',
-    requestedAt: req.reqestTime,
-    result: tours.length,
     data: {
-      tours
+      tours: 'return all tours'
     }
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find(el => el.id === id);
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  }
   res.status(200).json({
     status: 'succcess',
     data: {
-      tour
+      tour: 'return tour'
     }
   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: 'Create tour'
     }
-  );
+  });
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -17,29 +17,6 @@ mongoose
   })
   .catch(err => console.log(err));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'Test Tour 3',
-  price: 997,
-  rating: 4.6
-});
-
-testTour
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => console.log(err));
-
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
