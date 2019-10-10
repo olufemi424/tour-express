3b64bcee677bbbe36772ae496a88b3ec3d040691
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-09 21:02:18 -0500

    tour models

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,10 +4,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] }
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A  tour must have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //remove all the white space in the beginning and the end
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
