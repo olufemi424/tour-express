b4ef2e4e6bea51a1890e8336171d99e66f30d326
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-14 20:07:59 -0500

    alias middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,16 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratings,Average,summary,difficulty';
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // const tours = Tour.find()
     //   .where('duration')
     //   .equals(5)
