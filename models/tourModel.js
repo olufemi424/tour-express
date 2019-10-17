38fa86851c559e634629eea510e7637d744ae898
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-16 20:56:21 -0500

    mongoose document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const sligify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: { type: String },
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +65,28 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+// tourSchema.virtual('level').get(function() {
+//   if (this.difficulty === 'easy') return 'easy';
+//   if (this.difficulty === 'medium') return 'medium';
+//   if (this.difficulty === 'hard') return 'hard';
+// });
+
+//Document middleware: runs before the save() command and create()
+tourSchema.pre('save', function(next) {
+  this.slug = sligify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will Save Document');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
