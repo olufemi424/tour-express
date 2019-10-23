44442b2be4ecb56500bef44d3d2c0019cf9aa7f1
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-22 21:38:08 -0500

    error handling middlewaree

--- app.js
+++ app.js
@@ -29,9 +29,24 @@ app.use('/api/v1/users', userRouter);
 
 //not found error handling
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 400;
+
+  next(err);
+});
+
+//error handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
