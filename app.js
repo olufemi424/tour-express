71582c8b0b3d96fd5568412f5c4bae54fa79de6c
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-22 21:54:40 -0500

    global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -29,26 +31,18 @@ app.use('/api/v1/users', userRouter);
 
 //not found error handling
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 400;
-
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 400;
+  const error = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404
+  );
+  next(error);
 });
 
 //error handling
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 //Start Server
 module.exports = app;
