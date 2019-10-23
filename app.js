c513eb3cf3703382ece270a74db49d0fc9e0b02f
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-10-22 21:19:43 -0500

    not found error handling

--- app.js
+++ app.js
@@ -27,5 +27,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//not found error handling
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 //Start Server
 module.exports = app;
