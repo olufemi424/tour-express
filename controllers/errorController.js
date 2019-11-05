3f25ecaf9bb1ee175eb72b7863958dff80d7273a
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-11-04 20:15:37 -0600

    prod or dev error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,42 @@
 /*eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["err"] }]*/
 
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorForProd = (err, res) => {
+  // Operational, trusted error: Send message to client
+  if (err.isOpreational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programing or other unknown error: don't leak error detils
+  } else {
+    // 1.) Log Error
+    console.error('ERROR :', err);
+
+    //2.) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorForProd(err, res);
+  }
 };
