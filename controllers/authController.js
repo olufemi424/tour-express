1a4fa52cf5349aecff2564606d773fb9332f8b2a
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-11-14 21:23:18 -0600

    stateless authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,14 @@
 const jwt = require('jsonwebtoken');
-const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel');
+
+const AppError = require('../utils/appError');
+
+const singTonken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,9 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
+  const token = singTonken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -20,5 +26,33 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser
     }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1) check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  //2) check if the user exist  and password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+  //call User Model Instance method to reaturn a boolean
+  const isCorrect = user
+    ? await user.correctPassword(password, user.password)
+    : false;
+
+  if (!user || !isCorrect) {
+    return next(new AppError('Incorrect email or password'));
+  }
+
+  //3) if everything is ok, send token to the client
+  const token = singTonken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });
