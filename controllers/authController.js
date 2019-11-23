3cf2e07756f28fc2122d14876278db0f946311f2
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-11-22 23:37:24 -0600

    send cooking with response

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,22 @@ const singTonken = id => {
 // create send token to user, and the user will use this token to perform actions, this token contains user details
 const createSendToken = (user, statusCode, res) => {
   const token = singTonken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
 
+  //only want cooking in production
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+  //attach cooking to requests
+  res.cookie('jwt', token, cookieOptions);
+
+  //remove the password from the output
+  user.password = undefined; /*eslint no-param-reassign: "error"*/
   res.status(statusCode).json({
     status: 'success',
     token,
