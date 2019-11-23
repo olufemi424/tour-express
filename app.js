b85e86840bb470deb6ddd27389be45125a877557
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-11-23 00:21:43 -0600

    middleware to protect against HTTP Parameter Pollution attacks

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,6 +42,20 @@ app.use(mongoSanitize());
 
 //Data sanitization against XSS
 app.use(xss());
+
+// middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
