f499e6a637a243e49af3b32afd5e9a8db282818e
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-11-27 21:25:35 -0600

    review schema

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    tours: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      require: [true, 'Review must belong to a tour.']
+    },
+    users: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.']
+    }
+  },
+  {
+    //showup data as an output
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
