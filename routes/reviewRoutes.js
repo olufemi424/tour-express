0a9d58852c2fd8b2e5cfa0400dc51cc8afc7a49c
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-01-21 20:45:49 -0600

    cleanup authorization and authentication

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,9 @@ const authController = require('../controllers/authController');
 //mergePrams will take in incoming routes
 const router = express.Router({ mergeParams: true });
 
+//protect from un-authenticated user
+router.use(authController.protect);
+
 //POST /tour/243nm2/reviews
 //GET /tour/243nm2/reviews //get all reviews for a particular post
 //POST /reviews
@@ -21,7 +24,10 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
   .delete(
     authController.protect,
     authController.restrictTo('user', 'admin'),
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,33 +4,36 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+
+//protect other routes from un-authenticated user
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
 router.get(
   '/me',
   authController.protect,
   userController.getMe,
   userController.getUser
 );
-router.patch('/updateMe', authController.protect, userController.updateMe);
+router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-
-router.route('/').get(userController.getAllUsers);
+//protect routes from unauthorized user
+router.use(authController.restrictTo('admin'));
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(
-    authController.protect,
     authController.restrictTo('admin', 'user'),
     userController.deleteUser
   );
