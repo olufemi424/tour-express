0b10e609314600e0599c795f78396b6dc2effd37
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-02-29 19:37:43 -0600

    implementing map-box sdk

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,6 +103,7 @@ const tourSchema = new mongoose.Schema(
           default: 'Point',
           enum: ['Point']
         },
+        coordinates: [Number],
         address: String,
         description: String,
         day: Number
