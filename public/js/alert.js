baeb0ac1c7fbf3fd50be7eaa928c93337dd16004
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-05-24 21:08:31 -0500

    .

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 export const hideAlert = () => {
   const el = document.querySelector('.alert');
+  console.log(el);
   if (el) el.parentElement.removeChild(el);
 };
 
@@ -8,7 +9,7 @@ export const showAlert = (type, msg) => {
   hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-
+  m;
   //remove alert
   window.setTimeout(hideAlert, 5000);
 };
